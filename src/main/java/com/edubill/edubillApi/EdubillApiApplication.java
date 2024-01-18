1ef7debfcf14dc0f19e0feecc9c263db2fe2dceb
parkjaehak <atom8426@naver.com>
2024-01-18 17:50:11 +0900

    init api

---
+++ src/main/java/com/edubill/edubillApi/EdubillApiApplication.java
@@ -0,0 +1,13 @@
+package com.edubill.edubillApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdubillApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdubillApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edubill/edubillApi/EdubillApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edubill.edubillApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdubillApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

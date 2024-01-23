6c89767d0a7552abcf978bd05f3c2a387c236d17
parkjaehak <atom8426@naver.com>
2024-01-23 14:00:18 +0900

    feat. user CRUD작성, BaseEntity적용

--- src/main/java/com/edubill/edubillApi/EdubillApiApplication.java
+++ src/main/java/com/edubill/edubillApi/EdubillApiApplication.java
@@ -2,8 +2,10 @@ package com.edubill.edubillApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA Auditing을 활성화
 public class EdubillApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/edubill/edubillApi/domain/User.java
+++ src/main/java/com/edubill/edubillApi/domain/User.java
@@ -1,23 +1,53 @@
 package com.edubill.edubillApi.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+@Table(name = "USERS")
+public class User extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(name = "user_name", nullable = false)
     private String userName;
+
+    @Column(name = "user_email", unique = true, nullable = false)
+    private String userEmail;
+
+    @Column(name = "user_password", nullable = false)
+    private String userPassword;
+
+    @Column(name = "phone_number")
+    private Long phoneNumber;
+
+    @Column(name = "user_role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL) //user 삭제시 payment도 삭제
+    private List<Payment> payments = new ArrayList<>();
+
+
+
+//    @Column(name = "created_date")
+//    private LocalDateTime createdDate; // 가입날짜
+//
+//    @Column(name = "created_date")
+//    private LocalDateTime updatedDate; // 수정날짜
 
     public User(String userName) {
         this.userName = userName;

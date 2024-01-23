b90ccdcfe9e17571b4a7f4fe22d2a4bbe12cfd3b
parkjaehak <atom8426@naver.com>
2024-01-23 17:08:26 +0900

    feat. spring security적용, ErrorCode추가, AuthController추가

--- src/main/java/com/edubill/edubillApi/dto/UserDto.java
+++ src/main/java/com/edubill/edubillApi/dto/UserDto.java
@@ -1,16 +1,4 @@
 package com.edubill.edubillApi.dto;
 
-import jakarta.persistence.Column;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Setter
 public class UserDto {
-
-    private String userName;
-    private String userEmail;
-    private Long phoneNumber;
 }

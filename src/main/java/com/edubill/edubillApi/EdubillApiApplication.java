639be33819882024e6cfaf900bf91f421258680c
parkjaehak <atom8426@naver.com>
2024-01-24 17:41:37 +0900

    Revert "feat. jwt 설정 준비"
    This reverts commit 02cb64e36000a2996d57980dac59520377cf0f5b.

--- src/main/java/com/edubill/edubillApi/EdubillApiApplication.java
+++ src/main/java/com/edubill/edubillApi/EdubillApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing // JPA Auditing을 활성
+@EnableJpaAuditing // JPA Auditing을 활성화
 public class EdubillApiApplication {
 
 	public static void main(String[] args) {

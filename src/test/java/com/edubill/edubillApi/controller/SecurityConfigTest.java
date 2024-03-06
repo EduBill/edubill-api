def129dedfeb55c8a781cf469a61da9787988430
parkjaehak <atom8426@naver.com>
2024-03-06 12:45:12 +0900

    회원가입 및 로그인 통합 테스트로 변경

--- src/test/java/com/edubill/edubillApi/controller/SecurityConfigTest.java
+++ src/test/java/com/edubill/edubillApi/controller/SecurityConfigTest.java
@@ -3,9 +3,12 @@ package com.edubill.edubillApi.controller;
 import com.edubill.edubillApi.domain.User;
 import com.edubill.edubillApi.dto.user.LoginRequestDto;
 import com.edubill.edubillApi.dto.user.SignupRequestDto;
+import com.edubill.edubillApi.dto.verification.VerificationRequestDto;
+import com.edubill.edubillApi.dto.verification.VerificationResponseDto;
 import com.edubill.edubillApi.jwt.JwtProvider;
 import com.edubill.edubillApi.repository.UserRepository;
 
+import com.edubill.edubillApi.service.AuthService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 
@@ -13,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -24,8 +28,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.edubill.edubillApi.domain.UserRole.ACADEMY;
 import static com.edubill.edubillApi.domain.UserRole.ADMIN;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -42,37 +48,48 @@ public class SecurityConfigTest {
     private JwtProvider jwtProvider;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private @Qualifier("authServiceMock") AuthService authService;
 
 
     @Test
-    @Commit
-    @DisplayName("회원가입 테스트")
-    void signupTest() throws Exception {
+    @DisplayName("회원가입 후 로그인 진행")
+    void signupAndLoginTest() throws Exception {
+        //== 인증번호 발급 ==//
+        String phoneNumber = "01011111111";
+        VerificationResponseDto verificationResponse = authService.sendVerificationNumber(phoneNumber);
+        String requestId = verificationResponse.getRequestId();
+
+        //== 인증번호 검증 ==//
+        VerificationRequestDto verificationRequest = new VerificationRequestDto(requestId, "123456", "01011111111");
+        mockMvc.perform(post("/v1/auth/verify")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(verificationRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(requestId));
+
+        //== 회원가입 ==//
         // given
-        SignupRequestDto signupRequestDto = new SignupRequestDto("dfsdfsdf", "jaehak", "01087329001");
+        SignupRequestDto signupRequestDto = new SignupRequestDto(requestId, "edubill", "01011111111");
 
         // when
         mockMvc.perform(MockMvcRequestBuilders.post("/v1/auth/signup")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(signupRequestDto)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.userName").value("jaehak"))
-                .andExpect(jsonPath("$.phoneNumber").value("01087329001"))
-                .andExpect(jsonPath("$.userRole").value("ADMIN")); // 수정 필요
+                .andExpect(jsonPath("$.userName").value("edubill"))
+                .andExpect(jsonPath("$.phoneNumber").value("01011111111"))
+                .andExpect(jsonPath("$.userRole").value("ACADEMY")); // 수정 필요
 
         // then
-        User savedUser = userRepository.findByPhoneNumber("01087329001").orElse(null);
-        assertEquals("jaehak", savedUser.getUserName());
-        assertEquals(ADMIN, savedUser.getUserRole());
-    }
+        User savedUser = userRepository.findByPhoneNumber("01011111111").orElse(null);
+        assertEquals("edubill", savedUser.getUserName());
+        assertEquals(ACADEMY, savedUser.getUserRole());
 
-    @Test
-    @DisplayName("로그인 시 토큰 발급 및 auth 테스트")
-    void generateTokenAuthTest() throws Exception {
 
-        //== generateToken==//
+        //== 로그인 및 토큰 생성 ==//
         // given
-        LoginRequestDto loginRequestDto = new LoginRequestDto("dfsdfsdf", "01087329001");
+        LoginRequestDto loginRequestDto = new LoginRequestDto(requestId, "01011111111");
 
         // when
         ResultActions result = mockMvc.perform(MockMvcRequestBuilders.post("/v1/auth/login")
@@ -80,21 +97,20 @@ public class SecurityConfigTest {
                         .content(objectMapper.writeValueAsString(loginRequestDto)))
                 .andExpect(status().isOk())
                 .andExpect(header().exists(HttpHeaders.AUTHORIZATION))
-                .andExpect(jsonPath("$.accessToken").isNotEmpty());
+                .andExpect(jsonPath("$.jwtToken.accessToken").isNotEmpty());
         String bearerToken = result.andReturn().getResponse().getHeader(HttpHeaders.AUTHORIZATION);
         String accessToken = bearerToken.substring(7);
 
         // then
         assertTrue(jwtProvider.validateToken(accessToken));
         Claims userInfo = jwtProvider.getUserInfoFromToken(accessToken);
-        assertEquals("01087329001", userInfo.getSubject());
+        assertEquals("01011111111", userInfo.getSubject());
 
 
-        //==auth check==//
+        //== 토큰 인증 ==//
         mockMvc.perform(MockMvcRequestBuilders.get("/health")
                         .header(HttpHeaders.AUTHORIZATION, bearerToken))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello world"));
     }
-
 }

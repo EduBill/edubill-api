c9d4a81949da5f62c1b12f9a5b1eb2823bd5ff55
baezzys <wlsdn3578@gmail.com>
2024-04-04 00:14:58 +0900

    myprofile api swagger 설명 추가

--- src/main/java/com/edubill/edubillApi/controller/UserController.java
+++ src/main/java/com/edubill/edubillApi/controller/UserController.java
@@ -3,6 +3,7 @@ package com.edubill.edubillApi.controller;
 import com.edubill.edubillApi.domain.User;
 import com.edubill.edubillApi.user.response.UserProfileDto;
 import com.edubill.edubillApi.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "마이 프로필 가져오기", description = "회원 이름과 회원 type을 가져온다.")
     @GetMapping("/my-profile")
     public ResponseEntity<UserProfileDto> getUserProfile(final Principal principal) {
         final String userId = principal.getName();

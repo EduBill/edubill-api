45f13d1cd98b4c6711452f76ef58ec629436cb1a
baezzys <wlsdn3578@gmail.com>
2024-04-20 23:58:58 +0900

    납부현황 API에 납부 금액 과 미 납부금액 합계 추가

--- src/main/java/com/edubill/edubillApi/payment/response/PaymentStatusDto.java
+++ src/main/java/com/edubill/edubillApi/payment/response/PaymentStatusDto.java
@@ -2,6 +2,10 @@ package com.edubill.edubillApi.payment.response;
 
 public record PaymentStatusDto(
         Long paidCount,   // 납부 완료 인원
-        Long unpaidCount   // 미 납부 인원
+        Long unpaidCount,   // 미 납부 인원
+
+        Long totalPaidAmount,
+
+        Long totalunPaidAmount
 ) {
 }

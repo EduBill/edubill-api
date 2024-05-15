b7aa464ec0308914864a6972261f93db0cd7821a
parkjaehak <atom8426@naver.com>
2024-05-15 11:43:37 +0900

    student mock 데이터 생성 API

--- src/main/java/com/edubill/edubillApi/domain/Student.java
+++ src/main/java/com/edubill/edubillApi/domain/Student.java
@@ -1,8 +1,8 @@
 package com.edubill.edubillApi.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,24 +10,33 @@ import java.util.List;
 @Entity
 @Getter
 @Table(name = "STUDENTS")
-public class Student {
-
-    protected Student() {
-
-    }
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class Student extends BaseEntity{ //TODO: flyway수정
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "student_id")
     private Long id;
 
+    //TODO: flyway 수정
     @Column(name = "student_name")
     private String studentName;
 
-    @Column(name = "student_number")
-    private String studentNumber;
+    @Column(name = "student_phone_number")
+    private String studentPhoneNumber;
 
-    private Long studentGroupId;
+    @Column(name = "parent_name")
+    private String parentName;
+
+    @Column(name = "parent_phone_number")
+    private String parentPhoneNumber;
+
+    //TODO: flyway수정
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_group_id")
+    private StudentGroup studentGroup;
 
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private List<AcademyStudent> academyStudents = new ArrayList<>();

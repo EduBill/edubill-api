88ec0800ce054003b378a765bdaf007b43f40eb7
parkjaehak <atom8426@naver.com>
2024-05-15 12:32:48 +0900

    학생 mock 데이터 추가 API swagger 설정

--- src/main/java/com/edubill/edubillApi/controller/StudentController.java
+++ src/main/java/com/edubill/edubillApi/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.edubill.edubillApi.domain.User;
 import com.edubill.edubillApi.dto.StudentInfoRequestDto;
 import com.edubill.edubillApi.dto.user.UserProfileDto;
 import com.edubill.edubillApi.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,8 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Operation(summary = "학생 Mock데이터 생성",
+            description = "학정 정보를 등록한다.")
     @PostMapping("/test/generateStudentInfo")
     public ResponseEntity<String> addStudentInfo(
             @Validated @RequestBody  StudentInfoRequestDto studentInfoRequestDto,

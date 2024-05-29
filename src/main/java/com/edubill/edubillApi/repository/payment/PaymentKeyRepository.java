1dcc8e16cf10651a799a506eb1b65dbc120cdee6
parkjaehak <atom8426@naver.com>
2024-05-29 13:29:08 +0900

    결제키의 존재 유무 및 동일한지 검증하는 로직 추가

--- src/main/java/com/edubill/edubillApi/repository/payment/PaymentKeyRepository.java
+++ src/main/java/com/edubill/edubillApi/repository/payment/PaymentKeyRepository.java
@@ -1,7 +1,11 @@
 package com.edubill.edubillApi.repository.payment;
 
 import com.edubill.edubillApi.domain.PaymentKey;
+import com.edubill.edubillApi.domain.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentKeyRepository extends JpaRepository<PaymentKey, Long> {
+    List<PaymentKey> findAllByStudent(Student student);
 }

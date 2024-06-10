a757f8af674691712fe2be03b73c22991faabb2d
dlwlals1289 <dlwlals1289@gmail.com>
2024-06-10 15:56:22 +0900

    feat : 미납 학생 - 미확인 입금내역 연결하여 완납 수동 처리

---
+++ src/main/java/com/edubill/edubillApi/dto/payment/UnpaidHistoryRequestDto.java
@@ -0,0 +1,13 @@
+package com.edubill.edubillApi.dto.payment;
+
+import lombok.*;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UnpaidHistoryRequestDto {
+
+    private Long studentId;
+    private Long paymentHistoryId;
+}

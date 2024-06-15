664058301a711823af324917d2a32b524ec7a8e9
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2024-06-15 23:48:35 +0900

    fix: jar 파일 이름 변경하기 위한 쉘 스크립트 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,19 @@
 source /home/ubuntu/.bash_profile
 BUILD_JAR=$(ls /home/ubuntu/dev/action/build/libs/edubillApi-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/dev/action/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/dev/action/deploy.log
+# 파일 이름 변경을 위한 접미사 추가
+ENV_SUFFIX="-production"  # 또는 "-test" 등 환경에 맞게 조정
+NEW_JAR_NAME="${JAR_NAME%.jar}${ENV_SUFFIX}.jar"
+
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/dev/action/deploy.log
+echo "> 변경된 파일명: $NEW_JAR_NAME" >> /home/ubuntu/dev/action/deploy.log
+
 DEPLOY_PATH=/home/ubuntu/dev/action/
-cp $BUILD_JAR $DEPLOY_PATH
+cp $BUILD_JAR $DEPLOY_PATH$NEW_JAR_NAME
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/dev/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -f $NEW_JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
@@ -20,8 +25,9 @@ else
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+DEPLOY_JAR=$DEPLOY_PATH$NEW_JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/dev/action/deploy.log
 JAVA_OPTS="--spring.profiles.active=$SPRING_DEV_PROFILE --server.port=8081"
 
 nohup java -jar $DEPLOY_JAR $JAVA_OPTS >> /home/ubuntu/dev/deploy.log 2>> /home/ubuntu/dev/action/deploy_err.log &
+

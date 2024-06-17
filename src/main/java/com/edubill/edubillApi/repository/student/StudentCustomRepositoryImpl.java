4993148ffe29f9fc012947a25977dd77bda763b1
parkjaehak <atom8426@naver.com>
2024-06-17 15:18:33 +0900

    add: 중복된 이름, 유니크한 이름을 각각 리스트로 조회하는 querydsl 추가

--- src/main/java/com/edubill/edubillApi/repository/student/StudentCustomRepositoryImpl.java
+++ src/main/java/com/edubill/edubillApi/repository/student/StudentCustomRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.edubill.edubillApi.repository.student;
 
+import com.edubill.edubillApi.domain.QStudent;
 import com.edubill.edubillApi.domain.Student;
+import com.edubill.edubillApi.domain.StudentGroup;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.time.YearMonth;
+import java.util.Collection;
 import java.util.List;
 
 import static com.edubill.edubillApi.domain.QPaymentHistory.paymentHistory;
@@ -40,6 +44,38 @@ public class StudentCustomRepositoryImpl implements StudentCustomRepository{
                 .select(student)
                 .from(student, paymentHistory)
                 .where(student.parentName.eq(paymentHistory.depositorName))
+                .fetch();
+    }
+
+    @Override
+    public List<Student> findStudentsWithDuplicateNames(Collection<StudentGroup> studentGroups) {
+        QStudent student = QStudent.student;
+
+        return queryFactory
+                .selectFrom(student)
+                .where(student.studentGroup.in(studentGroups)
+                        .and(student.studentName.in(
+                                JPAExpressions.select(student.studentName)
+                                        .from(student)
+                                        .groupBy(student.studentName)
+                                        .having(student.count().gt(1))
+                        )))
+                .fetch();
+    }
+
+    @Override
+    public List<Student> findStudentsWithUniqueNames(Collection<StudentGroup> studentGroups) {
+        QStudent student = QStudent.student;
+
+        return queryFactory
+                .selectFrom(student)
+                .where(student.studentGroup.in(studentGroups)
+                        .and(student.studentName.in(
+                                JPAExpressions.select(student.studentName)
+                                        .from(student)
+                                        .groupBy(student.studentName)
+                                        .having(student.count().eq(1L))
+                        )))
                 .fetch();
     }
 }

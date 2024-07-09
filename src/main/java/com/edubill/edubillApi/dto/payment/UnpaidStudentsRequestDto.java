6dcb9b422dbef9b6bdd3b37f89c06e15e0a71325
dlwlals1289 <dlwlals1289@gmail.com>
2024-07-09 21:33:07 +0900

    add : requestDto & responseDto @Schema 추가

--- src/main/java/com/edubill/edubillApi/dto/payment/UnpaidStudentsRequestDto.java
+++ src/main/java/com/edubill/edubillApi/dto/payment/UnpaidStudentsRequestDto.java
@@ -1,5 +1,6 @@
 package com.edubill.edubillApi.dto.payment;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +14,9 @@ import java.time.YearMonth;
 @NoArgsConstructor
 public class UnpaidStudentsRequestDto {
 
+    @Schema(description = "userId", type = "long", example = "1")
     private String userId;
 
+    @Schema(description = "yearMonth", type = "string", pattern = "^\\d{4}-\\d{2}$", example = "2024-04")
     private YearMonth yearMonth;
 }

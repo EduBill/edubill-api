8998e3e367f66da85db48bb891619508ccc72c1a
parkjaehak <atom8426@naver.com>
2024-07-10 08:23:03 +0900

    modify: studentPaymentHistory와 paymentHistory의 기존 제약조건 drop 후 새로운 제약조건 생성

--- src/main/resources/db/migration/V1202407100004__add_cascade_in_studentpaymenthistory_table.sql
+++ src/main/resources/db/migration/V1202407100004__add_cascade_in_studentpaymenthistory_table.sql
@@ -1,5 +1,9 @@
+ALTER TABLE student_payment_history
+    DROP FOREIGN KEY IF EXISTS FK2lwbkvot1hyluov2rdtplgyre;
+
+
 alter table if exists student_payment_history
-    add constraint FK_my_custom_fk_name
+    add constraint FK2lwbkvot1hyluov2rdtplgyre
     foreign key (payment_history_id)
     references payment_history (payment_history_id)
-    on delete cascade
+    on delete cascade;

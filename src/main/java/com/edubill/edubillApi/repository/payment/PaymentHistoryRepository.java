e54169812c911635dbe06d7a527630d029ac98ec
parkjaehak <atom8426@naver.com>
2024-07-16 21:39:48 +0900

    Feat: 특정연월에 대해 생성된 엑셀 재업로드 시 overwrite

--- src/main/java/com/edubill/edubillApi/repository/payment/PaymentHistoryRepository.java
+++ src/main/java/com/edubill/edubillApi/repository/payment/PaymentHistoryRepository.java
@@ -3,5 +3,10 @@ package com.edubill.edubillApi.repository.payment;
 import com.edubill.edubillApi.domain.PaymentHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Optional;
+
 public interface PaymentHistoryRepository extends JpaRepository<PaymentHistory, Long>, PaymentHistoryCustomRepository {
+    Optional<PaymentHistory> findByDepositDateAndDepositorNameAndBankName(LocalDateTime depositDate, String depositorName, String bankName);
 }

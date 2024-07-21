5431e1c4f39409f14d55bba3717eb8ee5605a4b5
parkjaehak <atom8426@naver.com>
2024-07-21 19:18:03 +0900

    add: @Builder.Default 추가하여 studentGroups 초기화

--- src/main/java/com/edubill/edubillApi/error/exception/GroupNotFoundException.java
+++ src/main/java/com/edubill/edubillApi/error/exception/GroupNotFoundException.java
@@ -2,8 +2,8 @@ package com.edubill.edubillApi.error.exception;
 
 import com.edubill.edubillApi.error.ErrorCode;
 
-public class StudentGroupNotFoundException extends BusinessException {
-    public StudentGroupNotFoundException(String message) {
+public class GroupNotFoundException extends BusinessException {
+    public GroupNotFoundException(String message) {
         super(message, ErrorCode.GROUP_NOT_FOUND);
     }
 }

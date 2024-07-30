6d81b74c0f2dcedd8b528cb6497ebd7316e58737
parkjaehak <atom8426@naver.com>
2024-07-30 14:32:01 +0900

    Fix: 미납 학생 조회 PathVariable 사용

--- src/main/java/com/edubill/edubillApi/controller/PaymentController.java
+++ src/main/java/com/edubill/edubillApi/controller/PaymentController.java
@@ -183,10 +183,9 @@ public class PaymentController {
 
     @Operation(summary = "미납 학생 조회하기",
             description = "조회하는 월에 미납한 학생들을 조회한다.")
-    @GetMapping("/unpaidStudents")
-    public ResponseEntity<List<UnpaidStudentsResponseDto>> unPaidStudents(@RequestBody UnpaidStudentsRequestDto unpaidStudentsRequestDto){
+    @GetMapping("/unpaidStudents/{yearMonth}")
+    public ResponseEntity<List<UnpaidStudentsResponseDto>> unPaidStudents(@PathVariable(name = "yearMonth") YearMonth yearMonth){
         String userId = SecurityUtils.getCurrentUserId();
-        YearMonth yearMonth = unpaidStudentsRequestDto.getYearMonth();
 
         return ResponseEntity.ok(paymentService.getUnpaidStudentsList(userId, yearMonth));
     }

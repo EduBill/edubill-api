87aa7847d3fa9bb4bac7795353de94bbc0161c4c
parkjaehak <atom8426@naver.com>
2024-08-11 15:48:56 +0900

    add: 스테이징 서버 등록

--- src/main/java/com/edubill/edubillApi/config/security/CorsConfig.java
+++ src/main/java/com/edubill/edubillApi/config/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8203", "https://www.edu-bill.co.kr", "http://localhost:8080", "https://api.edu-bill.co.kr/")
+                        .allowedOrigins("https://staging.api.edu-bill.co.kr/","http://localhost:8203", "https://www.edu-bill.co.kr", "http://localhost:8080", "https://api.edu-bill.co.kr/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);

086c30d15a4a6f07322b275f344a378195aa94af
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2024-08-12 20:33:57 +0900

    Modify: dev ci/cd script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-source /home/ubuntu/.bash_profile
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/edubillApi-0.0.1-SNAPSHOT.jar)
+source /home/ubuntu/dev/.bash_profile
+BUILD_JAR=$(ls /home/ubuntu/dev/build/libs/edubillApi-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/dev/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
+echo "> build 파일 복사" >> /home/ubuntu/dev/deploy.log
+DEPLOY_PATH=/home/ubuntu/dev/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/dev/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/dev/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,7 +21,7 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/dev/deploy.log
 JAVA_OPTS="--spring.profiles.active=$SPRING_PROFILE"
 
-nohup java -jar $DEPLOY_JAR $JAVA_OPTS >> /home/ubuntu/deploy.log 2>> /home/ubuntu/action/deploy_err.log &
+nohup java -jar $DEPLOY_JAR $JAVA_OPTS >> /home/ubuntu/deploy.log 2>> /home/ubuntu/dev/deploy_err.log &

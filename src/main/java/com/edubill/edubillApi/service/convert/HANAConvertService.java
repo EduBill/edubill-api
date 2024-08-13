47143e967a882f68ccc25eb20df16802b4da8647
dlwlals1289 <dlwlals1289@gmail.com>
2024-08-13 14:39:17 +0900

    feat : 하나은행 엑셀 데이터 paymentHistory 객체로 변환 로직

--- src/main/java/com/edubill/edubillApi/service/convert/HANAConvertService.java
+++ src/main/java/com/edubill/edubillApi/service/convert/HANAConvertService.java
@@ -1,19 +1,85 @@
 package com.edubill.edubillApi.service.convert;
 
+import com.edubill.edubillApi.Validator;
 import com.edubill.edubillApi.domain.PaymentHistory;
 
+import com.edubill.edubillApi.domain.enums.PaymentType;
+import com.edubill.edubillApi.dto.payment.PaymentHistoryDto;
 import com.edubill.edubillApi.service.convert.ConvertService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service("HANAconvertService")
 public class HANAConvertService implements ConvertService {
+
+    private static final String BANK_NAME = "HANA";
+    private final Validator validator;
+
     @Override
     public List<PaymentHistory> convertBankExcelDataToPaymentHistory(MultipartFile file, String userId) throws IOException {
 
-        return null;
+        String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
+        Workbook workbook = null;
+        List<PaymentHistory> paymentHistories = new ArrayList<>();
+
+        if (fileExtension.equals("xls")) {
+            workbook = new HSSFWorkbook(file.getInputStream());
+        } else if (fileExtension.equals("xlsx")) {
+            workbook = new XSSFWorkbook(file.getInputStream());
+        } else {
+            throw new IllegalArgumentException("지원되지 않는 파일 형식입니다. xls 및 xlsx 파일만 지원됩니다.");
+        }
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (int rowNumber = 6; rowNumber < sheet.getLastRowNum(); rowNumber++) {
+
+            DataFormatter formatter = new DataFormatter();
+            Row row = sheet.getRow(rowNumber);
+
+            // 거래날짜
+            String originalDateTime = formatter.formatCellValue(row.getCell(0));
+            LocalDateTime depositDateTime = LocalDateTime.parse(originalDateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+            // 입금액
+            Cell depositAmountCell = row.getCell(4);
+            int depositAmount = (int) depositAmountCell.getNumericCellValue();
+
+            // 보낸분(입금자)
+            // 하나은행 같은 경우 입금자명 칸이 따로 없이 적요에 기재되어 있음
+            String depositorName = "";
+            if (depositAmount > 0) {
+                // 입금액이 양수인 경우에만 처리
+                depositorName = formatter.formatCellValue(row.getCell(2));
+                // 영어가,포함된 경우 해당 행의 데이터를 전달하지 않음
+                if (!validator.isValidDepositorName(depositorName)) {
+                    continue;
+                }
+            } else {
+                continue;
+            }
+            // 메모 - 하나은행은 메모 존재 하지 않고 추가 메모 시 적요 + 추가메모 형식으로 저장됨
+            String memo = "";
+
+            PaymentHistoryDto paymentHistoryDto = new PaymentHistoryDto(depositDateTime, depositorName, BANK_NAME, depositAmount, memo);
+
+            PaymentHistory paymentHistory = PaymentHistory.toEntity(paymentHistoryDto, PaymentType.BANK_TRANSFER, userId);
+            paymentHistories.add(paymentHistory);
+        }
+
+        return paymentHistories;
     }
 }
---
+++ src/test/java/com/edubill/edubillApi/service/ExcelServiceTest.java
@@ -0,0 +1,71 @@
+package com.edubill.edubillApi.service;
+
+import com.edubill.edubillApi.config.TestcontainerConfig;
+import com.edubill.edubillApi.domain.PaymentHistory;
+import com.edubill.edubillApi.domain.User;
+import com.edubill.edubillApi.service.convert.HANAConvertService;
+import com.edubill.edubillApi.service.convert.SHINHANConvertService;
+import com.edubill.edubillApi.service.excel.ExcelServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Slf4j
+@TestcontainerConfig
+public class ExcelServiceTest {
+
+    @Autowired
+    ExcelServiceImpl excelService;
+    @Autowired
+    HANAConvertService hanaConvertService;
+    @Autowired
+    SHINHANConvertService shinhanConvertService;
+
+    private MockMultipartFile getMockMultipartFile(String fileName, String contentType, String path) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(new File(path));
+        return new MockMultipartFile(fileName, fileName + "." + contentType, contentType, fileInputStream);
+    }
+
+    @Test
+    @DisplayName("하나은행 엑셀 데이터 업로드")
+    void convertBankDataToPaymentHistory_Hana() throws IOException {
+
+        //given
+        User user = User.builder()
+                .userId("1")
+                .build();
+        String fileName = "testHanaUpload";
+        String contentType = "xls";
+        String filePath = "src/test/resources/testHanaUpload.xls";
+        MockMultipartFile mockMultipartFile = getMockMultipartFile(fileName, contentType, filePath);
+
+        //when
+        List<PaymentHistory> paymentHistories = hanaConvertService.convertBankExcelDataToPaymentHistory(mockMultipartFile, user.getUserId());
+
+        //then
+        assertThat(paymentHistories.size()).isEqualTo(8);
+        assertThat(paymentHistories.get(0).getBankName()).isEqualTo("HANA");
+        assertThat(paymentHistories.get(0).getDepositorName()).isEqualTo("홍길동 (테스트테스트용)");
+        assertThat(paymentHistories.get(0).getPaidAmount()).isEqualTo(200000);
+    }
+
+}

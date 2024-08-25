4aaa0bb999d3c0480b993131539fcfbba5e6b283
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2024-08-25 18:01:09 +0900

    modify: swagger config 변경 및 properties 파일 swagger base url 추가

--- src/main/java/com/edubill/edubillApi/config/SwaggerConfig.java
+++ src/main/java/com/edubill/edubillApi/config/SwaggerConfig.java
@@ -10,30 +10,40 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 
 @OpenAPIDefinition(
         info = @Info(title = "Edubill dev Project",
                 description = "EdubillProject dev API 명세입니다.",
-                version = "v1"),
-        servers = {@Server(url = "https://dev-api.edu-bill.co.kr")}
+                version = "v1")
 )
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server.baseUrl}")
+    private String swaggerServerBaseUrl;
+
     @Bean
     public OpenAPI jwtApi(){
         SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER).name("Authorization");
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(Arrays.asList(securityRequirement));
+                .security(Collections.singletonList(securityRequirement))
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server().url(swaggerServerBaseUrl));
     }
 }

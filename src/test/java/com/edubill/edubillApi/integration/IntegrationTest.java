536cfc4cb1091aaa3bc8dce48e8f4f7c47d4dab7
yunhwan-dev <wjsdbsghks96@dgu.ac.kr>
2024-10-29 05:21:34 +0900

    Feat: database clean up 추가

--- src/test/java/com/edubill/edubillApi/integration/IntegrationTest.java
+++ src/test/java/com/edubill/edubillApi/integration/IntegrationTest.java
@@ -1,18 +1,49 @@
 package com.edubill.edubillApi.integration;
 
-import com.edubill.edubillApi.config.TestcontainerConfig;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestcontainerConfig
+@Import(DatabaseCleanUp.class)
+@ActiveProfiles("test")
 public class IntegrationTest {
 
     @LocalServerPort
     private int port;
+
+    private static final String ROOT = "root";
+    private static final String ROOT_PASSWORD = "";
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanUp;
+
+    @Container
+    protected static MariaDBContainer container;
+
+    @DynamicPropertySource
+    private static void configureProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", () -> ROOT);
+        registry.add("spring.datasource.password", () -> ROOT_PASSWORD);
+    }
+
+    static {
+        container = new MariaDBContainer("mariadb:11.2.2")
+                .withDatabaseName("edubill")
+                .withUsername(ROOT)
+                .withPassword(ROOT_PASSWORD);
+        container.start();
+    }
 
 
     @BeforeEach
@@ -21,5 +52,9 @@ public class IntegrationTest {
         if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
             RestAssured.port = port;
         }
+
+        databaseCleanUp.clear();
+
     }
 }
+
